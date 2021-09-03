d6c5a5e88bc1ffe0e97af862c979258e12a0e73f
Rashwan <Ahmed.Rashwan2014@yahoo.com>
2021-09-03 19:18:12 +0200

    ignore node_modules

--- services/comment/index.js
+++ services/comment/index.js
@@ -2,22 +2,25 @@ const express= require("express")
 const bodyParser = require("body-parser")
 const {randomBytes} = require("crypto")
 const app = express()
-const comments={};
+const commentsByPostId={};
 
 app.use(bodyParser.json())
 /**
  * 
  */
-app.get("/comments",(req,res)=>{
- res.send(posts);
+app.get("/posts/:id/comments",(req,res)=>{
+ res.send(commentsByPostId[req.params.id] || []);
 })
 /**
  * 
  */
-app.post("/comments",(req,res)=>{
- const id = randomBytes(4).toString("hex");
- comments[id] = {id,title:req.body.title};
- res.status(201).send(comments[id])
+app.post("/posts/:id/comments",(req,res)=>{
+    const   commentId = randomBytes(4).toString("hex");
+    const {content} = req.body;
+    const comments = commentsByPostId[req.params.id] || [];
+    comments.push({id:commentId, content});
+    commentsByPostId[req.params.id] = comments;
+    res.status(201).send(commentsByPostId[req.params.id])
 })
 
 /**
