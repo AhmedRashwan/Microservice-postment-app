9d041cd18c35faa526318569ff77ef5a195ae998
Rashwan <Ahmed.Rashwan2014@yahoo.com>
2021-12-04 14:11:40 +0200

    add client basic arctechture

---
+++ client/src/CommentCreate.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const CommentCreate = ({ postId }) => {
+  const [content, setContent] = useState("");
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const result = await axios.post(
+      `http://localhost:5000/posts/${postId}/comments`,
+      {
+        content: content,
+      }
+    );
+    console.log(result);
+  };
+  return (
+    <div className="form-group">
+      <form onSubmit={onSubmit}>
+        <label>Create Comment</label>
+        <input
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+          className="form-control"
+        />
+        <button className="btn btn-success" type="submit">
+          CreateComment
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default CommentCreate;
---
+++ client/src/CommentList.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const CommentList = ({ postId }) => {
+  const [comments, setComments] = useState({});
+
+  const fetchComments = async () => {
+    let res = await axios.get(`http://localhost:5000/posts/${postId}/comments`);
+    setComments(res.data);
+  };
+
+  useEffect(() => {
+    fetchComments();
+  }, []);
+
+  const renderComments = Object.values(comments).map((comment) => {
+    return <li key={comment.id}>{comment.content}</li>;
+  });
+  return <ul>{renderComments}</ul>;
+};
+
+export default CommentList;
